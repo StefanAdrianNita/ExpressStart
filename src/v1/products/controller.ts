b5ab26351283823afc6352abfcd16494c60ecb57
Stefan Adrian Nita <stefanadrian.nita@icloud.com>
2023-05-23 17:20:59 +0200

    minor fixes

--- src/v1/products/controller.ts
+++ src/v1/products/controller.ts
@@ -4,8 +4,8 @@ import { getAllProducts } from "./services/allProducts";
 const allProducts = async (req: any, res: any) => {
   console.log(" [Server]: called /v1/products/ (allProducts)");
   getAllProducts().then((data) => {
-    console.log(data);
-    res.json(data);
+    res.statusCode = 200;
+    res.json({ data });
   });
 };
 module.exports = {
